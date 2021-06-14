be6c58207e1ed551cff2c325d1ea11becf693395
alexandre antonio <alexandre.pisani.ant@gmail.com>
2021-06-14 16:11:28 -0300

    heroku deploy

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/container/latest/",
+    publicPath: "/container/public",
   },
   plugins: [
     new ModuleFederationPlugin({
