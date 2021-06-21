1e119d7f4f5ada6c78d3a757ecafcf42a7db361f
alexandre antonio <alexandre.pisani.ant@gmail.com>
2021-06-21 14:04:37 -0300

    mfe

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    // publicPath: "/container/",
+    publicPath: "app/container/dist",
   },
   plugins: [
     new ModuleFederationPlugin({
